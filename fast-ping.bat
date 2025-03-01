4818a5acc0377da70178f3b09a00f3b9562be35b
IGOR10S <igorsamotoy29@gmail.com>
2025-03-01 01:00:50 +0100

    Script

--- fast-ping.bat
+++ fast-ping.bat
@@ -3,16 +3,14 @@ setlocal enabledelayedexpansion
 
 :: Controlla se è stato passato un file con gli indirizzi IP
 if "%~1"=="" (
-    echo Usage: %~nx0 fast_ip.txt
-    echo The file must contain one IP per line.
-    pause
+    echo Usage: .\%~nx0 .\name_file.txt
+    echo NOTE: The file must contain one IP/Hostname per line.
     exit /b
 )
 
 :: Controlla se il file esiste
 if not exist "%~1" (
     echo The file %~1 does not exist.
-    pause
     exit /b
 )
 
@@ -20,18 +18,21 @@ if not exist "%~1" (
 > fast_report.txt echo Ping results:
 echo ---------------------------------- >> fast_report.txt
 
+set count=1
+
 :: Legge gli indirizzi IP dal file e li pinga uno a uno
 for /f "usebackq tokens=*" %%I in ("%~1") do (
-    echo Pinging %%I ...
-    ping %%I -n 4 -w 2000 > nul
+    echo [!count!] Pinging "%%I" ...
+    ping "%%I" -n 4 -w 2000 > nul
     if errorlevel 1 (
-        echo %%I - UNREACHABLE >> fast_report.txt
-        echo %%I - UNREACHABLE
+        echo     UNREACHABLE
+        echo UNREACHABLE - %%I >> fast_report.txt
     ) else (
-        echo %%I - REACHABLE >> fast_report.txt
-        echo %%I - REACHABLE
+        echo     REACHABLE
+        echo REACHABLE - %%I >> fast_report.txt
     )
     echo -------------------------
+    set /a count+=1
 )
 
 echo Report saved in fast_report.txt
